386a77b698d81749a7bf207645b986a6e946e797
Han4Star2 <hanneszeyringer@icloud.com>
2026-02-09 22:36:14 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,14 +20,7 @@ app.get("/", (req, res) => {
 
 res.redirect(`/?discordId=${discordId}`);
 
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Statische Dateien ausliefern
-app.use(express.static(__dirname)); 
 // ------------------------
 // Datenbank initialisieren
 // ------------------------
